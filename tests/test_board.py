4f09fccf480246d4d06b81eb2aa0f6139663762d
LeoJu06 <leo.junghans6@gmail.com>
2024-11-13 21:46:53 +0100

    corrected the board.py test

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,8 +1,8 @@
 from src.othello.board import Board
 import src.othello.game_constants as const
 
+
 def test_board():
-    
     # Init empty board
     board = Board(const.EMPTY_BOARD)
 
@@ -12,9 +12,10 @@ def test_board():
     # Check whether the starting positions are empty at the beginning and do not contain any player pieces
     for x, y in start_positions:
         assert board.board[x][y] == const.EMPTY_CELL, f"({x}|{y}) should be empty"
-        assert board.board[x][y] not in [const.PlayerColor.BLACK.value, const.PlayerColor.WHITE.value], (
-            f"({x}|{y}) should not be occupied by any player"
-        )
+        assert board.board[x][y] not in [
+            const.PlayerColor.BLACK.value,
+            const.PlayerColor.WHITE.value,
+        ], f"({x}|{y}) should not be occupied by any player"
 
         # Apply move
         board.apply_move(x, y)
@@ -26,7 +27,9 @@ def test_board():
     expected_moves = [(2, 3), (3, 2), (4, 5), (5, 4)]
 
     # Assertion to ensure that the calculated valid moves are correct
-    assert valid_moves == expected_moves, f"Expected valid moves {expected_moves}, but got {valid_moves}"
+    assert (
+        valid_moves == expected_moves
+    ), f"Expected valid moves {expected_moves}, but got {valid_moves}"
 
     # New game situation: An additional move that changes the game situation
     new_move = (2, 3)  # Beispielzug, der auf das Board angewendet wird
@@ -36,7 +39,9 @@ def test_board():
     new_valid_moves = board.valid_moves()
 
     # Expected valid movess after adding a peace
-    new_expected_moves = [(1, 3), (2, 2), (3, 5), (5, 4)]  
+    new_expected_moves = [(2, 2), (2, 4), (4, 2)]
 
     # Assertion for new board state
-    assert new_valid_moves == new_expected_moves, f"Expected new valid moves {new_expected_moves}, but got {new_valid_moves}"
+    assert (
+        new_valid_moves == new_expected_moves
+    ), f"Expected new valid moves {new_expected_moves}, but got {new_valid_moves}"
