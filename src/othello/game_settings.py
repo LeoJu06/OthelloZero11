7b9640d903e78c944923b85669786ce04666af5c
LeoJu06 <leo.junghans6@gmail.com>
2024-11-28 21:15:52 +0100

    Edditet variable name

--- src/othello/game_settings.py
+++ src/othello/game_settings.py
@@ -6,7 +6,7 @@ ROWS, COLS = 8, 8
 SQUARE_SIZE = HEIGHT // COLS
 
 # Define colors
-BG_COLOR = (50, 50, 50)  # Dark gray background
+BACKGROUND_COLOR = (50, 50, 50)  # Dark gray background
 GRID_COLOR = (200, 200, 200)  # Light gray for the grid lines
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
