e48600de7def376e5633d13aae86ec9645cdbae4
LeoJu06 <leo.junghans6@gmail.com>
2024-11-28 20:15:43 +0100

    Added file for holding game settingss

---
+++ src/othello/game_settings.py
@@ -0,0 +1,12 @@
+
+
+# Set the size of the game window
+WIDTH, HEIGHT = 1300, 900
+ROWS, COLS = 8, 8
+SQUARE_SIZE = HEIGHT // COLS
+
+# Define colors
+BG_COLOR = (50, 50, 50)  # Dark gray background
+GRID_COLOR = (200, 200, 200)  # Light gray for the grid lines
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
