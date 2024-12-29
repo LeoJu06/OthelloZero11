c0bd92046acd5984510f0f34d0c45b67e2de76a5
LeoJu06 <leo.junghans6@gmail.com>
2024-12-29 17:53:27 +0100

    Added hyperparameter

--- src/config/hyperparameters.py
+++ src/config/hyperparameters.py
@@ -1,7 +1,11 @@
 import torch
 
+
 class Hyperparameters:
-    MCTS = {"num_simulations": 50, "exploration_weight": 1.0}
+    MCTS = {"num_simulations": 10000, "exploration_weight": 1.0}
 
+    Neural_Network = {
+        "device": torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    }
 
-    Neural_Network = {"device": torch.device("cuda" if torch.cuda.is_available() else "cpu")}
+    Node = {"key_passing": -1, "prior_passing": 1}
