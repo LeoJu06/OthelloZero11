7c6a45f1f20f3d784d3b86d4f1614abdd4d37ed0
LeoJu06 <leo.junghans6@gmail.com>
2025-02-06 17:27:42 +0100

    added methods

--- src/mcts/coach.py
+++ src/mcts/coach.py
@@ -2,4 +2,13 @@
 
 class Coach:
 
-    pass
+
+    def __init__(self):
+        pass
+
+    def execute_episode(self):
+        pass
+
+
+    def train(self):
+        pass
--- src/mcts/pit.py
+++
@@ -1,4 +0,0 @@
-
-class Pit:
-
-    pass
