522579372d0548de88981864d41170a97fab8166
LeoJu06 <leo.junghans6@gmail.com>
2025-02-06 21:58:48 +0100

    introduced a datamanger class

---
+++ src/data_manager/data_manager.py
@@ -0,0 +1,30 @@
+from src.othello.game_constants import PlayerColor
+from src.othello.othello_game import OthelloGame
+import numpy as np
+from src.mcts.node import Node
+
+class DataManager:
+
+    def __init__(self):
+
+        self.data = []
+        self.game = OthelloGame()
+        
+        pass
+
+    def create_example(self, current_state, player, root:Node):
+        """Creates an example"""
+        
+        if player == PlayerColor.BLACK.value:
+            current_state = self.game.get_canonical_board(current_state, player)
+
+        return [current_state, root.pi()]
+    
+    def assign_rewards(self, examples)
+
+
+
+    def collect(self, training_example):
+        """Collects data examples"""
+
+        self.data.append(training_example)
