869d66132fee813a182f801247470187c82e5f65
LeoJu06 <leo.junghans6@gmail.com>
2025-02-11 21:57:09 +0100

    added new hyperparameter

--- src/config/hyperparameters.py
+++ src/config/hyperparameters.py
@@ -10,12 +10,12 @@ class Hyperparameters:
 
     The Hyperparameter class contains:
         - MCTS with keys ["num_simulations", "exploration_weight", "temp_threshold", temperature]
-        - Coach with keys ["iterations", "episodes", "num_workers"]
+        - Coach with keys ["iterations", "episodes", "num_workers", "episodes_per_worker"]
         - Neural_Network with keys ["device"]
         - Node with keys ["key_passsing, prior_passing]"""
 
     MCTS = {
-        "num_simulations": 700,
+        "num_simulations": 100,
         "exploration_weight": 1.0,
         "temp_threshold": 10,
         "temp": 1,
@@ -23,7 +23,8 @@ class Hyperparameters:
 
     Coach = {"iterations": 2, 
              "episodes": 2500,
-             "num_workers" : 22}
+             "num_workers" :22, }
+    Coach["episodes_per_worker"] = Coach["episodes"] // Coach["num_workers"]
 
     Neural_Network = {
         "device": torch.device("cuda" if torch.cuda.is_available() else "cpu")
